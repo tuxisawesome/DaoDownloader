cb82ddbf99cc3b4e6fe06886f77f32ffee094aac
tuxisawesome <67063826+tuxisawesome@users.noreply.github.com>
2026-02-27 17:13:10 -0500

    Remove unnecessary line from BinEcho.py

--- system/apps/BinEcho.py
+++ system/apps/BinEcho.py
@@ -1,5 +1,4 @@
 #1.1
-
 def init(drivers, drivernames, configmgr, drivermgr,kernel):
     display = drivers[drivernames.index("display")]
     argv = configmgr.getvalue(configmgr.readconfig("env.cfg"), "argv")
